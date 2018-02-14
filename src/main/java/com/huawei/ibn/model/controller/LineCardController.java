81b2d7ab96fe655ae973c84b8a929c37a127c0cd
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2018-02-14 09:48:21 +0200

    2 nodes and a relationship

---
+++ src/main/java/com/huawei/ibn/model/controller/LineCardController.java
@@ -0,0 +1,10 @@
+package com.huawei.ibn.model.controller;
+
+import com.huawei.ibn.model.physical.LineCard;
+import org.springframework.data.neo4j.repository.GraphRepository;
+
+public interface LineCardController extends GraphRepository<LineCard> {
+
+
+
+}
--- src/main/java/com/huawei/ibn/web/DeviceWebController.java
+++ src/main/java/com/huawei/ibn/web/DeviceWebController.java
@@ -1,8 +1,12 @@
 package com.huawei.ibn.web;
 
 import com.huawei.ibn.model.controller.DeviceController;
+import com.huawei.ibn.model.controller.LineCardController;
 import com.huawei.ibn.model.physical.Device;
+import com.huawei.ibn.model.physical.LineCard;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -13,6 +17,9 @@ public class DeviceWebController {
     @Autowired
     private DeviceController deviceController;
 
+    @Autowired
+    private LineCardController lineCardController;
+
     @RequestMapping(value = "/device", method = RequestMethod.GET)
     @ResponseBody
     List<Device> getAllDevices(){
@@ -21,13 +28,39 @@ public class DeviceWebController {
 
     @RequestMapping(value = "/device/{name}", method = RequestMethod.POST)
     @ResponseBody
-    String addDevice(@PathVariable  String name) {
+    void addDevice(@PathVariable  String name) {
         Device device = new Device();
         device.setName(name);
         deviceController.save(device);
-
-        return "done";
     }
 
+    @RequestMapping(value = "/device/{name}/linecard/{index}", method = RequestMethod.POST)
+    @ResponseStatus
+    ResponseEntity addLineCardToDevice(@PathVariable String name, @PathVariable String index) {
+
+        Device device = deviceController.findByName(name);
+        if (device==null){
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        LineCard lineCard = new LineCard();
+        lineCard.setName(index);
+
+        lineCardController.save(lineCard);
+
+        device.setLineCard(lineCard);
+
+        deviceController.save(device);
+
+        return new ResponseEntity(HttpStatus.OK);
+
+    }
+
+    @RequestMapping(value = "/device", method = RequestMethod.POST)
+    @ResponseStatus
+    ResponseEntity<Device> createDevice(@RequestBody Device device){
+        Device save = deviceController.save(device);
+        return new ResponseEntity<Device>(save, HttpStatus.OK);
+    }
 
 }

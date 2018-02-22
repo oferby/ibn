68dfd805bef823c5fe5b82ca7fd2bbbab542f288
oferby <shtoza00>
2018-02-22 17:05:03 +0200

    gRPC with spring boot fixes

--- src/main/java/com/huawei/ibn/Application.java
+++ src/main/java/com/huawei/ibn/Application.java
@@ -17,11 +17,6 @@ public class Application {
 
     public static void main(String[] args) throws IOException {
 
-//        Server server = ServerBuilder.forPort(8081)
-//                .addService(new GreetingServiceImpl())
-//                .build();
-//        server.start();
-
         SpringApplication.run(Application.class, args);
 
     }
--- src/main/java/com/huawei/ibn/grpc/GreetingServiceImpl.java
+++ src/main/java/com/huawei/ibn/grpc/GreetingServiceImpl.java
@@ -1,17 +1,29 @@
 package com.huawei.ibn.grpc;
 
+import com.huawei.ibn.model.controller.DeviceController;
+import com.huawei.ibn.model.physical.Device;
 import com.test.tutorial.GreetingServiceGrpc;
 import com.test.tutorial.HelloRequest;
 import com.test.tutorial.HelloResponse;
 import io.grpc.stub.StreamObserver;
 import org.lognet.springboot.grpc.GRpcService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @GRpcService
-public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase{
+public class GreetingServiceImpl extends GreetingServiceGrpc.GreetingServiceImplBase {
+
+
+    @Autowired
+    private DeviceController deviceController;
+
+
     @Override
     public void greeting(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
 
-        HelloResponse helloResponse = HelloResponse.newBuilder().setResponse("hello " + request.getRequest()).build();
+        Device device = deviceController.findAll().iterator().next();
+        HelloResponse helloResponse = HelloResponse.newBuilder()
+                .setResponse("found device: " + device.getName())
+                .build();
 
         responseObserver.onNext(helloResponse);
         responseObserver.onCompleted();
